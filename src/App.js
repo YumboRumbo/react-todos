6d688dabec65e616ae71429c70ced25eebd7c8cf
Sang Yum <sangyum@gmail.com>
2017-02-08 21:14:08 -0800

    broke the app into smaller components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Header from './Header'
+import Footer from './Footer'
+import Content from './Content'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Content />
+        <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
